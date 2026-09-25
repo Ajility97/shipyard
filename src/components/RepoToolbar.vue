83181adf8b64c34b564609723b6dc55ccd71a69d
Philip Theobald <fylzero@gmail.com>
2026-09-24 20:57:19 -0500

    move undo unpushed before fetch so the sync buttons stay put

--- src/components/RepoToolbar.vue
+++ src/components/RepoToolbar.vue
@@ -351,6 +351,20 @@ async function toggleBranches() {
     <div class="repo-toolbar-bar repo-toolbar-actions">
       <div class="repo-toolbar-work">
         <button
+          v-if="canUndoUnpushed"
+          class="ghost tiny"
+          type="button"
+          :disabled="busy"
+          :title="undoUnpushedTitle"
+          @click="emit('undoUnpushed')"
+        >
+          <svg class="button-icon" viewBox="0 0 24 24" aria-hidden="true">
+            <path d="M9 15L3 9m0 0l6-6M3 9h10.5a6 6 0 010 12H12" />
+          </svg>
+          Undo unpushed
+          <span class="file-count-badge">{{ unpushedCount }}</span>
+        </button>
+        <button
           class="ghost tiny"
           type="button"
           :disabled="busy"
@@ -391,20 +405,6 @@ async function toggleBranches() {
             />
           </svg>
           Push
-        </button>
-        <button
-          v-if="canUndoUnpushed"
-          class="ghost tiny"
-          type="button"
-          :disabled="busy"
-          :title="undoUnpushedTitle"
-          @click="emit('undoUnpushed')"
-        >
-          <svg class="button-icon" viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M9 15L3 9m0 0l6-6M3 9h10.5a6 6 0 010 12H12" />
-          </svg>
-          Undo unpushed
-          <span class="file-count-badge">{{ unpushedCount }}</span>
         </button>
       </div>
     </div>
